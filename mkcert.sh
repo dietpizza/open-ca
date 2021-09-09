c32d9011b8531912abc8de49d34c9bc818b62cf2
Rohan Biswas <brainhacker550@gmail.com>
2021-09-10 02:36:27 +0530

    updated logic

--- mkcert.sh
+++ mkcert.sh
@@ -1,44 +1,58 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]
+shopt -s nullglob
+shopt -s nocasematch
+
+if [ $# -lt 1 ]
 then 
-   echo "Usage: $0 <cert-name> <ca-name>"
+   echo "Usage: $0 <cert-name>"
    exit 1
 fi
-
-NAME=$1
-DIR="certs/$NAME"
-FILE="$DIR/$NAME"
-
-CA_NAME=$2
-CA_DIR="ca/$CA_NAME"
-CA_FILE="$CA_DIR/$CA_NAME"
 
 DOMAINS="domains.ext"
 
 function mkca () {
-    mkdir -p $CA_DIR
+    CA_NAME=$1
+    CA_DIR="ca/$CA_NAME"
+    CA_FILE="$CA_DIR/$CA_NAME"
 
-    openssl req\
-        -x509\
-        -nodes\
-        -new\
-        -sha256\
-        -days 3650\
-        -newkey rsa:2048\
-        -keyout "$CA_DIR/$CA_NAME.key"\
-        -out "$CA_DIR/$CA_NAME.pem"\
-        -subj "/C=IN/O=$CA_NAME/OU=Enginnering/CN=$CA_NAME"
+    if [ ! -d $CA_DIR ]; then
+        read -n1 -p "The certificate authority '$CA_NAME' does not exist. Create it now? [y/n]: " CHOICE
+        echo
 
-    openssl x509\
-        -outform pem\
-        -in "$CA_DIR/$CA_NAME.pem"\
-        -out "$CA_DIR/$CA_NAME.crt"
-    echo
-    echo "Certificate Authority '$CA_NAME' created."
+        if [[ $CHOICE == "y" ]]; then
+            mkdir -p $CA_DIR
+
+            openssl req\
+                -x509\
+                -nodes\
+                -new\
+                -sha256\
+                -days 3650\
+                -newkey rsa:2048\
+                -keyout "$CA_DIR/$CA_NAME.key"\
+                -out "$CA_DIR/$CA_NAME.pem"\
+                -subj "/C=IN/O=$CA_NAME/OU=Enginnering/CN=$CA_NAME"
+
+            openssl x509\
+                -outform pem\
+                -in "$CA_DIR/$CA_NAME.pem"\
+                -out "$CA_DIR/$CA_NAME.crt"
+
+            echo
+            echo "Certificate Authority '$CA_NAME' created."
+        else
+            echo "Create a certificate authority first."
+            exit 1
+        fi
+    fi
 }
 
 function mkcert () {
+    NAME=$1
+    DIR="certs/$NAME"
+    FILE="$DIR/$NAME"
+
     mkdir -p $DIR
 
     openssl req\
@@ -52,11 +66,11 @@ function mkcert () {
     cp $DOMAINS $DIR/
 
     read -p "Enter domain names (eg. echo.org *.echo.org): " -a DNS
-    read -p "Enter IPs (eg. 127.0.0.1 192.168.56.128): " -a IP
-
     for i in ${!DNS[@]}; do
         echo "DNS.$(($i + 1)) = ${DNS[$i]}" >> $DIR/$DOMAINS
     done
+
+    read -p "Enter IPs (eg. 127.0.0.1 192.168.56.128): " -a IP
     for i in ${!IP[@]}; do
         echo "IP.$(($i + 1)) = ${IP[$i]}" >> $DIR/$DOMAINS
     done
@@ -82,22 +96,16 @@ function mkcert () {
     echo "Certificate '$NAME' created."
 }
 
-if [ -d $DIR ] 
-then
-    echo "Certificate '$NAME' already exists -_-"
+if [ -d certs/$1 ]; then
+    echo "Certificate '$1' already exists -_-"
     exit 1
 else
-    if [ ! -d $CA_DIR ]
-    then
-        read -n1 -p "The Authority '$CA_NAME' does not exist. Create it now? [y/n]: " CHOICE
-        echo
-        case $CHOICE in 
-            [Yy] ) mkca;;
-            [Nn] ) exit 1;;
-            * ) echo "Please answer yes or no."; exit 1;;
-        esac
-    fi
+    LIST=(ca/*)
+    echo "Certificate Authorities Available:"
+    printf '%s\n' "${LIST[@]##*/}"
+    read -p "Enter CA name: " CA_NAME
 
-    mkcert
+    mkca $CA_NAME
+    mkcert $1
 fi
 
